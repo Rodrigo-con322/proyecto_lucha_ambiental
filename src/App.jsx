a29c25665a7342f596d697ca0f1b8abf7b5b63d2
AlexVM479 <alexvergaramalpartida46@gmail.com>
2025-06-18 14:10:46 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
-      <Route path="/Login" element={<Login />} />
+      <Route path="/Inicio/Login" element={<Login />} />
     </Routes>
   );
 }
